01a9d782b8b620c4416cd4c6026f5005fc0f1fda
chrisGrando <christianjulio2010@gmail.com>
2023-02-07 19:24:43 -0300

    Command line arguments

--- src/AppLogic.py
+++ src/AppLogic.py
@@ -13,39 +13,41 @@ class AppLogic:
         self.args = args
         self.fullClassPath = os.path.realpath(os.path.dirname(__file__))
 
+    # Obtém caminho absoluto do arquivo
+    def getAbsPathFile(self, path):
+        finalPath = None
+
+        # Se o caminho já for absoluto, mantém como está
+        if (os.path.isabs(path)):
+            finalPath = path
+        # Se o caminho for relativo, converte para absoluto
+        else:
+            finalPath = self.fullClassPath + "/" + path
+        
+        return finalPath
+
     # Executa o programa
     def run(self):
+        # Checa se os parâmetros de linha de comando estão corretos
+        if (len(self.args) != 3):
+            print("Uso incorreto de parâmetros.\nSaindo...")
+            exit(0)
+
+        # Caminho para o arquivo da tabela de AFD
+        tablePath = self.getAbsPathFile(self.args[1])
+
+        # Caminho para o arquivo do script
+        scriptPath = self.getAbsPathFile(self.args[2])
+
         # Lê o arquivo do Autômato Finito Determinístico
         cr = CsvReader()
-        cr.read(self.fullClassPath + "/data/afd.csv")
+        cr.read(tablePath)
         table = cr.getTableData()
 
         # Gera a fita de entrada
         tape = Tape(table)
-        inputTape = tape.generateTape(self.fullClassPath + "/data/script.cdc")
+        inputTape = tape.generateTape(scriptPath)
 
-        # Depurar...
-        self.debugMyApp(
-            table,
-            tape.readCode(self.fullClassPath + "/data/script.cdc"),
-            inputTape
-        )
-
-    # Função para testes
-    def debugMyApp(self, table, code, tape):
-        # Parâmetros de linha de comando
-        print("*** PARÂMETROS ***")
-        print(self.args)
-
-        # Tabela de AFD
-        print("\n*** AFD ***")
-        for row in table:
-            print(row)
-
-        # Código fonte
-        print("\n*** SCRIPT ***")
-        print(code, end='')
-
-        # Fita de entrada
+        # Exibe a fita
         print("*** FITA ***")
-        print(tape)
+        print(inputTape)
